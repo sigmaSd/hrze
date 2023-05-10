7e7db15f600e87eacbfc17370b62dc938ad2d6ce
Nbiba Bedis <bedisnbiba@gmail.com>
2023-05-10 22:54:23 +0100

    hrze

---
+++ dist/server.ts
@@ -0,0 +1,6 @@
+import { serveDir } from "https://deno.land/std@0.186.0/http/file_server.ts";
+import { serve } from "https://deno.land/std@0.186.0/http/server.ts";
+
+serve((req) => {
+  return serveDir(req);
+});
