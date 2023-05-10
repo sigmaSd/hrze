19f7fb3e133532a546b0b17966713b4e36a870b1
Nbiba Bedis <bedisnbiba@gmail.com>
2023-05-10 22:59:37 +0100

    deploy

--- dist/server.ts
+++ dist/server.ts
@@ -1,7 +1,8 @@
 import { serveDir } from "https://deno.land/std@0.186.0/http/file_server.ts";
 import { serve } from "https://deno.land/std@0.186.0/http/server.ts";
 
-Deno.chdir("./dist");
 serve((req) => {
-  return serveDir(req);
+  return serveDir(req, {
+    fsRoot: "./dist",
+  });
 });
